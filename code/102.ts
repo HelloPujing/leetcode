8b6bd1ae9147c8f051fd6f257b1324ac7a8047b4
pujing <pj2332@gmail.com>
2023-05-22 10:37:55 +0800

    refactor

--- code/102.ts
+++ code/102.ts
@@ -1,13 +1,13 @@
 /* 
 二叉树的层序遍历
 分析：
-逐层，BFS
-
+逐层，BFS，队列实现
 */
 
 import { TreeNode } from "./type/treeNode";
 
 function levelOrder(root: TreeNode<number> | null): number[][] {
+    
     return [[]];
 };
 
