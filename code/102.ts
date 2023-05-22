1e10131c379d1a57bb3b55e06d7f282198b6dd0c
pujing <pj2332@gmail.com>
2023-05-22 12:14:06 +0800

    102

--- code/102.ts
+++ code/102.ts
@@ -7,7 +7,24 @@
 import { TreeNode } from "../type/treeNode";
 
 function levelOrder(root: TreeNode<number> | null): number[][] {
-    
-    return [[]];
+    const ans: any[] = [];
+    if(!root) return ans;
+
+    const q = [];
+    q.push(root);
+    while(q.length) {
+        ans.push([]);
+        const currentLevelSize = q.length; // 巧妙之处在于每次循环前就取出大小，这样就可以继续往q里塞值
+        const currentIndex = ans.length - 1; // 巧妙之处在于每次循环都已经定位好index
+        for (let i = 0; i < currentLevelSize; i++) {
+            const node = q.shift();
+            ans[currentIndex].push(node?.val);
+            if(node?.left) q.push(node.left);
+            if(node?.right) q.push(node.right);
+        }
+    }
+
+    return ans;
 };
 
+levelOrder(null);
