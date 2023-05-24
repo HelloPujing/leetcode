46bff96180cd76aa2c2a1fa94c4d4c71575277fc
pujing <pj2332@gmail.com>
2023-05-24 12:19:47 +0800

    88

--- doc/88.ts
+++ doc/88.ts
@@ -1,5 +1,5 @@
 /* 合并两个有序数组 */
-
+// O(m+n) O(m)
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
     const arr1 = nums1.slice(0, m);
     for (let index = 0, i = 0, j = 0; index < nums1.length; index++) {
@@ -15,3 +15,22 @@ function merge(nums1: number[], m: number, nums2: number[], n: number): void {
         }
     }
 };
+
+// 从后往前插入，直到nums2都插完
+function merge2(nums1: number[], m: number, nums2: number[], n: number): void {
+    for (let index = m + n - 1, i = m - 1, j = n - 1; j >= 0; index--) {
+        const a = nums1[i];
+        const b = nums2[j];
+
+        if(j === -1 || a >= b) {
+            nums1[index] = a;
+            i--;
+        }else if(i === -1 || b > a){
+            nums1[index] = b;
+            j--;
+        }
+        console.log(nums1);
+    }
+};
+
+merge2([1,2,3,0,0,0], 3, [2,5,6], 3);
