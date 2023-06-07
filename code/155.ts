b43f514832155abc9b546f553a12ab8f9b4b7de9
pujing <pj2332@gmail.com>
2023-06-07 16:19:29 +0800

    155

--- code/155.ts
+++ code/155.ts
@@ -1,6 +1,6 @@
 /* 
 最小栈
-
+同步栈，存储最小值
 
 */
 
@@ -32,11 +32,30 @@ class MinStack {
     }
 }
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * var obj = new MinStack()
- * obj.push(val)
- * obj.pop()
- * var param_3 = obj.top()
- * var param_4 = obj.getMin()
- */
+class MinStack2 {
+    stack_min: number[];
+    stack_x: number[];
+    constructor() {
+        this.stack_min = new Array();
+        this.stack_x = new Array();
+    }
+
+    push(val: number): void {
+        const len = this.stack_x.length;
+        this.stack_x.push(val);
+        this.stack_min.push(this.stack_min.length > 0 ? Math.min(val, this.stack_min[len - 1], val) : val);
+    }
+
+    pop(): void {
+        this.stack_x.pop();
+        this.stack_min.pop();
+    }
+
+    top(): number {
+        return this.stack_x[this.stack_x.length - 1];
+    }
+    
+    getMin(): number {
+        return this.stack_min[this.stack_min.length - 1];
+    }
+}
